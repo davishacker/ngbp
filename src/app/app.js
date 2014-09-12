1a9d25d6fd18fcd0c28bfb05e2d51fca9b525509
daihua ye <davishacker86@gmail.com>
2014-09-12 00:53:15 -0700

    template for start a new project

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,6 @@ angular.module( 'ngBoilerplate', [
   'templates-app',
   'templates-common',
   'ngBoilerplate.home',
-  'ngBoilerplate.about',
   'ui.router'
 ])
 
@@ -14,12 +13,4 @@ angular.module( 'ngBoilerplate', [
 })
 
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
-    }
-  });
-})
-
-;
-
+});
